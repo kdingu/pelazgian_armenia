ac60ddc6bc596891e0d3ee7fc807d12da2459f9e
Klaidi Dingu <klaididingu@gmail.com>
2025-01-20 21:56:52 +0100

    vk

--- app/root.tsx
+++ app/root.tsx
@@ -37,7 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <Meta />
       <Links />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
             rel="stylesheet" />
     </head>
